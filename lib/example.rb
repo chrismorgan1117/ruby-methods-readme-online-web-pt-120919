6c8c097bc0204f05a4f1eb0d6ea9e74247a8f21d
Christopher Morgan <cwm111794@gmail.com>
2019-12-09 18:27:59 +0000

    Automatically backed up by Learn

--- lib/example.rb
+++ lib/example.rb
@@ -16,10 +16,11 @@ def say_hello_ten_times
   puts phrase
   puts phrase
   puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+end 
